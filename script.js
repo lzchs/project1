f9a75129f5bdc4f470100c7b2668c34e6b46af11
lzchs <lyziches@gmail.com>
2026-03-24 21:24:32 +0200

    add new files

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleAnswer(id){
+const answer = document.getElementById(id);
+if (answer.style.display === 'none' || !answer.style.display) { answer.style.display = 'block';
+} else {
+answer.style.display = 'none'
+}
+}
